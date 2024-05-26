25ca03387caa31ab91e0a928acebee83be3dd6a5
Luca-Caporaloni <lucacaporaloni@gmail.com>
2024-05-26 18:55:38 -0300

    Todo listo

--- Assets/Scripts/Nuevos Scripts/Controller_Player_StaticAir.cs
+++ Assets/Scripts/Nuevos Scripts/Controller_Player_StaticAir.cs
@@ -5,30 +5,40 @@ using UnityEngine;
 
 public class Controller_Player_StaticAir : Controller_Player
 {
+
     private bool isStatic = false;
 
-    // Update is called once per frame
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            isStatic = !isStatic;
-            if (isStatic)
-            {
-                rb.velocity = Vector3.zero; // Detiene el movimiento actual del personaje
-                rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation; // Congela la posición en todos los ejes
-            }
-            else
-            {
-                rb.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation; // Restablece las restricciones de movimiento
-            }
+            ToggleFreeze();
         }
 
-        // Movimiento y salto solo si no está estático en el aire
         if (!isStatic)
         {
-            Movement(); //Pasé el void a public para poder usarlo en este script
-            Jump(); // Permitir el salto cuando no esté estático
+            Movement();
         }
     }
+
+    void ToggleFreeze()
+    {
+        isStatic = !isStatic;
+
+        if (isStatic)
+        {
+            rb.velocity = Vector3.zero;
+            rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+        }
+        else
+        {
+            rb.constraints = RigidbodyConstraints.FreezeRotation;
+        }
+    }
+
 }
